fe66dac70fc43e576cfd4c1d4640cfa264f7e12c
Sukrit Khare <kharesukrit153@gmail.com>
2024-08-20 17:14:54 +0530

    Add class constructors

--- HotelManagementSystem.Entities/Class1.cs
+++ HotelManagementSystem.Entities/Class1.cs
@@ -42,6 +42,14 @@ namespace HotelManagementSystem.Entities
             get { return owner; }
             set { owner = value; }
         }
+        public Hotel(int id, int categoryId, string location, string contactDetails,string owner)
+        {
+            Id = id;
+            CategoryId = categoryId;
+            Location = location;
+            ContactDetails = contactDetails;
+            Owner = owner;
+        }
     }
 
     public class HotelCategory
@@ -59,64 +67,105 @@ namespace HotelManagementSystem.Entities
             get { return name; }
             set { name = value; }
         }
+        public HotelCategory(int id,string name)
+            {
+                Id= id;
+            Name= name;
+            }
+            
     }
+
+
+    public class Room
+    {
+        private int id;
+        public int ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        private int categoryID;
+        public int CategoryID
+        {
+            get { return categoryID; }
+            set { categoryID = value; }
+        }
+
+        private int rateID;
+
+        public int RateID
+        {
+            get { return rateID; }
+            set { rateID = value; }
+        }
+
+
+        public Room(int id,int categoryid,int rateid)
+        {
+            ID= id;
+            CategoryID = categoryid;
+            RateID = rateid;
+        }
+
+    }
+
+    public class RoomCategory
+    {
+        private int id;
+        public int ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        RoomCategory(int id,string name)
+        {
+            ID = id;
+            Name = name;
+        }
+
+
+    }
+
+    public class RoomRates
+    {
+
+        public int Id { get; set; }
+        public double Rate { get; set; }
+
+        public String UpdateDate { get; set; }
+        public int RoomCategoryId { get; set; }
+
+        public RoomRates(int id, double rate, string updateDate, int roomCategoryId)
+        {
+            Id = id;
+            Rate = rate;
+            UpdateDate = updateDate;
+            RoomCategoryId = roomCategoryId;
+        }
+    }
+    public class RoomInventory
+    {
+        public int CategoryId { get; set; }
+        public int AvailabilityOfRoom { get; set; }
+
+        public RoomInventory(int categoryId, int availabilityOfRoom)
+        {
+            CategoryId = categoryId;
+            AvailabilityOfRoom = availabilityOfRoom;
+        }
+    }
+
+
+
 }
 
-public class Room
-{
-    private int id;
-    public int ID
-    {
-        get { return id; }
-        set { id = value; }
-    }
 
-    private int categoryID;
-    public int CategoryID
-    {
-        get { return categoryID; }
-        set { categoryID = value; }
-    }
-
-    private int rateID;
-
-    public int RateID
-    {
-        get { return rateID; }
-        set { rateID = value; }
-    }
-
-
-    public Room()
-    {
-        id = 0;
-        categoryID = 0;
-        rateID = 0;
-    }
-
-}
-
-public class RoomCategory
-{
-    private int id;
-    public int ID
-    {
-        get { return id; }
-        set { id = value; }
-    }
-
-    private string name;
-    public string Name
-    {
-        get { return name; }
-        set { name = value; }
-    }
-
-    RoomCategory()
-    {
-        id = 0;
-        name = string.Empty;
-    }
-
-    static void Main(string[] args) { }
-}
